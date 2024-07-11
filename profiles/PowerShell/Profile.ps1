255e89d38d6fb84130b17ce4a0a5d86f340836a3
G0V1NDS <connect.govinds@gmail.com>
2024-07-11 19:49:30 +0530

    - Added MYVIMRCLSP env variable - Added aliases for dotfiles directories and updated nvim alias - Formatted file

--- profiles/PowerShell/Profile.ps1
+++ profiles/PowerShell/Profile.ps1
@@ -1,23 +1,26 @@
 # File for Current User, All Hosts - $PROFILE.CurrentUserAllHosts
-function New-Bash-Alias {
-    param (
-        [string]$name,
-        [string]$command
-    )
+function New-Bash-Alias
+{
+  param (
+    [string]$name,
+    [string]$command
+  )
 
-    $aliasPath = "Function:\global:$name"
+  $aliasPath = "Function:\global:$name"
 
-    # Check if the alias already exists
-    if (-not (Test-Path $aliasPath)) {
-        $sb = [scriptblock]::Create($command)
-        New-Item -Path $aliasPath -Value $sb | Out-Null
-        # Write-Output "Alias '$name' created: $command"
-    }
+  # Check if the alias already exists
+  if (-not (Test-Path $aliasPath))
+  {
+    $sb = [scriptblock]::Create($command)
+    New-Item -Path $aliasPath -Value $sb | Out-Null
+    # Write-Output "Alias '$name' created: $command"
+  }
 }
 
 # Show path to executable
-function which ($command) {
-    Get-Command -Name $command -ErrorAction SilentlyContinue |
+function which ($command)
+{
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
@@ -31,18 +34,20 @@ Import-Module -Name Terminal-Icons
 
 # Env variables
 $env:YAZI_FILE_ONE = "$Env:Programfiles\Git\usr\bin\file.exe"
-
+$env:MYVIMRCLSP = "$env:LOCALAPPDATA/nvim/lua/initlsp.lua"
 $env:EDITOR = "nvim"
 
 # Aliases
 Set-Alias vi nvim
 New-Bash-Alias pwsha "start-process pwsh -verb runas"
-New-Bash-Alias dot "cd ~/windots"
 New-Bash-Alias cf "cd ~/.config"
+New-Bash-Alias dot "cd ~/windots"
+New-Bash-Alias udot "dot; cd dotfiles-open;"
+New-Bash-Alias pdot "udot; cd Private;"
 New-Bash-Alias doti "dot; . ./install"
 New-Bash-Alias q "exit"
 New-Bash-Alias .. "cd ../"
-New-Bash-Alias c "nvim @args"
+New-Bash-Alias c "nvim -u $env:MYVIMRCLSP @args"
 New-Bash-Alias g "git @args"
 
 
@@ -58,9 +63,9 @@ Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory
 Set-PSReadLineKeyHandler -Chord RightArrow -Function ForwardWord
 Set-PSReadLineKeyHandler -Chord Tab -Function ForwardWord
 Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('yazi')
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+  [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+  [Microsoft.PowerShell.PSConsoleReadLine]::Insert('yazi')
+  [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
