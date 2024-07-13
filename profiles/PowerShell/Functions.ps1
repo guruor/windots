b4c9791c9e4b48ea00da281762efee271659dcdb
G0V1NDS <connect.govinds@gmail.com>
2024-07-13 13:47:57 +0530

    Added mappings to open/toggle common programs

--- profiles/PowerShell/Functions.ps1
+++ profiles/PowerShell/Functions.ps1
@@ -7,6 +7,33 @@ if (-not $env:MY_FUNCTIONS_LOADED)
       Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
   }
 
+  function openterm
+  {
+    [CmdletBinding()]
+    param(
+      [string]$title,
+      [string]$cmdStr = "$env:SHELL",
+      [string]$shell = "$env:SHELL",
+      [string]$workingDirectory = "$env:USERPROFILE"
+    )
+
+    $terminal = "alacritty"
+
+    # Check if specified terminal executable exists
+    switch ($terminal)
+    {
+      "alacritty"
+      {
+        Start-Process -FilePath alacritty -ArgumentList "--title ""$title"" --working-directory ""$workingDirectory"" -e ""$shell"" -c ""$cmdStr"""
+      }
+      default
+      {
+        Write-Warning "Unsupported terminal: $terminal"
+        return
+      }
+    }
+  }
+
   Write-Host "✅ Loaded functions successfully." -ForegroundColor Green
 } else
 {
