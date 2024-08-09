64bdabd232b3c52a1d73d5d1c04ff92319c228f4
G0V1NDS <connect.govinds@gmail.com>
2024-08-09 16:06:47 +0530

    Adding BINPATH to PATH Adding scoop and mpv binaries to PATH

--- profiles/PowerShell/EnvVariables.ps1
+++ profiles/PowerShell/EnvVariables.ps1
@@ -8,6 +8,23 @@ if (-not $env:MY_ENV_VARS_LOADED)
   # Use pwsh if not using WSL
   $env:SHELL = "bash"
 
+  $env:BINPATH="$env:USERPROFILE/.bin"
+  # Adding binaries from BINPATH to path
+  if ($env:Path -notlike "*$env:BINPATH*")
+  {
+    $env:Path += ";$env:BINPATH"
+  }
+
+  if ($env:Path -notlike "*$($env:USERPROFILE)\scoop\shims*")
+  {
+    $env:Path += ";$env:USERPROFILE\scoop\shims"
+  }
+
+  if ($env:Path -notlike "*$($env:USERPROFILE)\scoop\apps\mpv\current*")
+  {
+    $env:Path += ";$env:USERPROFILE\scoop\apps\mpv\current"
+  }
+
   Write-Host "✅ Loaded env variables successfully." -ForegroundColor Green
 } else
 {
